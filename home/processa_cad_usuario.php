1e3fff51446db83c712cab43e4e6c064e97ea18f
Nilson Tacito <nillmartins91@gmail.com>
2021-03-16 09:09:01 -0300

    Acertos landing page (pendente redirecionamento pós login)

--- home/processa_cad_usuario.php
+++ home/processa_cad_usuario.php
@@ -1,9 +1,7 @@
 <?php
-
-//pra onde o form mandou
-    //session_start();
+    //iniciar a sessão na página redirecionada?
     include_once("conexao.php");
-    $nome_usuario = $_POST['nome']; //cadê o sobrenome? kkk
+    $nome_usuario = $_POST['nome'];
     $sbnome_usuario = $_POST['sobrenome'];
     $documento_usuario = $_POST['documento'];
     $email_usuario = $_POST['email'];
@@ -12,31 +10,12 @@
     $endereco_usuario = $_POST['endereco'];
     $cep_usuario = $_POST['cep'];
     
-    //aqui rola o insert
-    $result_usuario = "INSERT INTO cliente(nome, sobrenome, documento, email, senha, telefone, endereco, cep) VALUES ('$nome_usuario','$sbnome_usuario','$documento_usuario','$email_usuario', '$senha_usuario','$telefone_usuario','$endereco_usuario','$cep_usuario')";
-    $resultado_usuario = mysqli_query($conn, $result_usuario);
-    //checar erro no insert
-    
-    //preciso disso fazendo um select no banco!!!  
-    
-    //aqui rola o select pros dados recém inputados
-    /*$query = "SELECT nome FROM cliente WHERE email = '{$email_usuario}' AND senha = '{$senha_usuario}'";
-    
-    $result = mysqli_query($conn, $query);
-    
-    $row = mysqli_num_rows($result);
-    
+    $query = "INSERT INTO cliente(documento, email, senha, nome, sobrenome, telefone, endereco, cep) VALUES ('$documento_usuario', '$email_usuario', '$senha_usuario', '$nome_usuario','$sbnome_usuario', '$telefone_usuario','$endereco_usuario','$cep_usuario')";
+    $resultado_usuario = mysqli_query($conn, $query);
+    //o código nao tem erros, nao sei pq redireciona
+    if($resultado_usuario != null) {
+        header('Location: /cliente/pages/mapa.php');
+        exit();
+    }
 
-    if($row == 1) {
-        $_SESSION['usuario'] = $nome_usuario;
-        header('Location: /tcc/crud/form-create.php');
-        exit();
-    } else {
-        $_SESSION['nao_autenticado'] = true;
-        header('Location: index.php');
-        exit();
-        }*/
-
-        header('Location: /tcc/dash/examples/cadastro-veiculo.php');//mandando pra tela do cad veículo
-        exit();
 ?>
